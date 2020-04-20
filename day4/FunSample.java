29909d1752181feb85ba98ea5d67b10b3a3a501f
kntejaswini38 <kntejaswini38@gmail.com>
2020-04-20 15:25:52 +0530

    Functions_AdditionOfTwoNumbers

---
+++ day4/FunSample.java
@@ -0,0 +1,13 @@
+public class FunSample {
+    int num1;
+    int num2;
+    static void SumOfNum(int num1,int num2){
+        int num3;
+        num3 = num1 + num2;
+        System.out.println("Sum of the numbers : " + num3);
+    }
+    public static void main(String[] args) {
+        SumOfNum(2,3);
+    }
+
+}
