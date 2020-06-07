0cce76e2af0ffa4f30b9274d18c0f8b64fcb2386
Avinash <avinash975@gmail.com>
2020-06-07 23:29:10 +0530

    Add files via upload

---
+++ day52/ListExample.java
@@ -0,0 +1,37 @@
+package org.aion;
+
+import avm.Address;
+import avm.Blockchain;
+import org.aion.avm.tooling.abi.Callable;
+import org.aion.avm.userlib.AionList;
+
+public class ListExample
+{
+    private static AionList<String> addressList = new AionList<>();
+
+
+    @Callable
+    public static void addAddress(Address newAddress) {
+        addressList.add(newAddress.toString());
+    }
+
+    @Callable
+    public static int getSize() {
+        return(addressList.size());
+    }
+
+    @Callable
+    public static String removeAddress(Address address) {
+        if (addressList.contains(address.toString())) {
+            addressList.remove(address.toString());
+            return ("address removed :"+ address.toString());
+        }
+        else
+        {
+            return("address not found in the list");
+        }
+
+    }
+
+
+}
