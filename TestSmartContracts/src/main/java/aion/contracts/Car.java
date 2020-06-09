b5dcde86fd3750d7281e03f47efd95e509201797
Harshil Bhagat <hrshlbhgt@gmail.com>
2020-06-09 23:08:09 +0530

    Assignment to create a mapping of Cars

---
+++ TestSmartContracts/src/main/java/aion/contracts/Car.java
@@ -0,0 +1,25 @@
+package aion.contracts;
+
+import org.aion.avm.tooling.abi.Callable;
+import org.aion.avm.userlib.AionMap;
+
+public class Car {
+    private static final AionMap<Integer, Car> cars = new AionMap<>();
+    private String make,model,color;
+
+    @Callable
+    public static void addCar(String make, String model, String color)
+    {Car newCar = new Car();
+        newCar.make = make;
+        newCar.model = model;
+        newCar.color = color;
+
+        cars.put(cars.size(), newCar);
+    }
+
+    @Callable
+    public static String carToFrontend(int id) {
+        return "Make: " + cars.get(id).make + " | Model: " + cars.get(id).model + " | Color: " + cars.get(id).color;
+    }
+
+}
