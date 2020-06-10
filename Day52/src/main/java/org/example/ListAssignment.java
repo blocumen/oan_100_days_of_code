19e14acdc4691151a533b87f71e19b74d0be9312
anuradha-mdg <anuradha.mudhigonda@gmail.com>
2020-06-10 23:34:32 +0530

    Token Assignment Example

--- Day52/src/main/java/org/example/ListAssignment.java
+++ Day52/src/main/java/org/example/ListAssignment.java
@@ -1,4 +1,32 @@
 package org.example;
+import avm.Blockchain;
+import org.aion.avm.tooling.abi.Callable;
+import avm.Address;
+import org.aion.avm.userlib.AionList;
 
 public class ListAssignment {
+
+    public static AionList  listOfAddress = new AionList();
+
+    @Callable
+    public static void setListOfAddress(Address address)
+    {
+        if(!listOfAddress.contains(address))
+            listOfAddress.add(address);
+        else
+            Blockchain.print("Address already in the list"+address);
+
+    }
+    @Callable
+    public static void removefromlist(Address address)
+    {
+        listOfAddress.remove(address);
+    }
+
+    @Callable
+    public static int listcount()
+    {
+        return listOfAddress.size();
+
+    }
 }
--- Day52/src/main/java/org/example/TokenAssignment.java
+++ Day52/src/main/java/org/example/TokenAssignment.java
@@ -1,4 +1,28 @@
 package org.example;
 
+import avm.Address;
+import avm.Blockchain;
+import org.aion.avm.tooling.abi.Callable;
+import org.aion.avm.userlib.AionMap;
+
 public class TokenAssignment {
+
+    public static AionMap<Address , Integer> balances = new AionMap();
+
+    @Callable
+    public static void assignToken(Address address,int token)
+    {
+        if(!balances.containsKey(address))
+            balances.put(address,token);
+        else
+            Blockchain.print("Address already assigned value" +address);
+    }
+    @Callable
+    public static int getBlances(Address address)
+    {
+        Blockchain.println("Balance of address" +address+" - "+ balances.get(address));
+        return balances.get(address);
+
+    }
+
 }
